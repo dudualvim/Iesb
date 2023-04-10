3d7dc9f01dc7685438ae96035d71b4cb8faf29b4
Eduardo Alvim <62016520+dudualvim@users.noreply.github.com>
2023-04-10 17:07:13 -0300

    Add files via upload

---
+++ Programacao_Paralela/Primos.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include <locale.h>
+ 
+int main() {
+ setlocale(LC_ALL, "");
+ int num, i, resultado = 0;
+ 
+ printf("Digite um numero: ");
+ scanf("%d", &num);
+ 
+ 
+ for (i = 2; i <= num / 2; i++) {
+    if (num % i == 0) {
+       resultado++;
+       break;
+    }
+ }
+ 
+ if (resultado == 0)
+    printf("%d e um numero primo\n", num);
+ else
+    printf("%d nao e um numero primo\n", num);
+ 
+ return 0;
+}
---
+++ Programacao_Paralela/Teste.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+int Divide(int,int);
+
+int main(int argc, char const *argv[])
+{
+    
+    int  i, *ponteiro;
+
+    printf("Digite o tamanho do vetor:");
+    scanf("%d", i);
+
+    int vet[i];
+
+    for(i=0; i<vet[i]; i++){
+        *ponteiro = vet[i] + 10;
+
+    };
+
+    printf("%d", &vet[i]);
+
+
+
+};
+
